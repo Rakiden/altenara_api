07465b3411315f8af76be872a98ed159253c6579
Rakiden <rakiden97@gmail.com>
2020-06-04 10:39:50 +0200

    changes

--- proses-api.php
+++ proses-api.php
@@ -9,8 +9,8 @@ header("Content-Type: application/json; charset=UTF-8");
 
 define('DB_NAME','altenara');
 define('DB_USER','root');
-define('DB_PASSWORD','');
-define('DB_HOST','localhost');
+define('DB_PASSWORD','pestillo');
+define('DB_HOST','google');
 
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
