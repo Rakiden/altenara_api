c62b29fbc2490906814cb63c881031008fc08baa
Rakiden <rakiden97@gmail.com>
2020-06-09 11:28:33 +0200

    test

--- proses-api.php
+++ proses-api.php
@@ -15,8 +15,16 @@ $dbname = DB_NAME;
 $dbuser = DB_USER;
 $dbpassword = DB_PASSWORD;
 $dbhost = DB_HOST;
-echo ($dbname);
-echo ($dbuser);
-echo ($dbhost);
-echo ($dbpassword);
+
+$postjson = json_decode(file_get_contents('php://input'),true);
+
+if($postjson['aksi']=="cargarProductos"){
+  $query = mysqli_query($mysqli, "SELECT * FROM ps_product_lang WHERE id_product = 1");
+
+
+  if($query) $result = json_encode(array('success'=>true));
+  else $result = json_encode(array('success'=>false, 'msg'=>"dbname: " + $dbname + " dbuser: " + $dbuser + " dbhost:" + $db_host));
+
+  echo $result;
+}
 ?>
