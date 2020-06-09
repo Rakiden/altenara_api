59fd71a51d15fbbc2ac30ea3af5b950e329fabf9
Rakiden <rakiden97@gmail.com>
2020-06-09 11:32:27 +0200

    test

--- proses-api.php
+++ proses-api.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 
-define('DB_NAME','Altenara');
+define('DB_NAME','altenara');
 define('DB_USER','root');
 define('DB_PASSWORD','pestillo');
 define('DB_HOST','localhost');
@@ -23,7 +23,7 @@ if($postjson['aksi']=="cargarProductos"){
 
 
   if($query) $result = json_encode(array('success'=>true));
-  else $result = json_encode(array('success'=>false, 'msg'=>"dbname: ".$dbname." dbuser: ".$dbuser." dbhost:".$db_host));
+  else $result = json_encode(array('success'=>false, 'msg'=>mysql_error()));
 
   echo $result;
 }
