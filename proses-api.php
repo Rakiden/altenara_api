f52b1323c41f5f950522f209e61cafe32bfee578
Rakiden <rakiden97@gmail.com>
2020-06-22 09:52:50 +0200

    test

--- proses-api.php
+++ proses-api.php
@@ -5,10 +5,10 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json; charset=UTF-8");
 
-define('DB_NAME','altenara');
-define('DB_USER','root');
-define('DB_PASSWORD','pestillo');
-define('DB_HOST','localhost/phpmyadmin');
+define('DB_NAME','altenara_bbdd');
+define('DB_USER','altenara_user');
+define('DB_PASSWORD','Y966T2Mz4Q)K');
+define('DB_HOST','befresh3.hospedados.net');
 
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
