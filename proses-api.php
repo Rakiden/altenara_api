0fb3588a9f89b26da2375fed55d630162b6118e0
Rakiden <rakiden97@gmail.com>
2020-06-22 10:43:46 +0200

    test

--- proses-api.php
+++ proses-api.php
@@ -15,17 +15,7 @@ $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 $postjson = json_decode(file_get_contents('php://input'),true);
 
 if($postjson['mode']=="cargarProductos"){
-  $query = mysqli_query($mysqli, "SELECT * FROM ps_product_lang WHERE id_product =1");
-  $data = array();
-
-  while($row=mysqli_fetch_array($query)){
-    array_push($data,$row);
-  }
-
-  if($query) $result = json_encode(array('success'=>true, 'products'=>$data));
-  else $result = json_encode(array('success'=>false, 'msg'=>"Error, intentelo de nuevo"));
-
-  echo $result;
+  echo json_encode(array('success'=>false, 'msg'=>"Error, intentelo de nuevo");
 }
 
 ?>
