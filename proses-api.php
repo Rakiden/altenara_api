706a3f2f62c42be0a0919a0322e0c0ed23afd00f
Rakiden <rakiden97@gmail.com>
2020-06-24 09:57:50 +0200

    test

--- proses-api.php
+++ proses-api.php
@@ -8,15 +8,17 @@ header("Content-Type: application/json; charset=UTF-8");
 define('DB_NAME','altenara_bbdd');
 define('DB_USER','altenara_user');
 define('DB_PASSWORD','Y966T2Mz4Q)K');
-define('DB_HOST','befresh3.hospedados.net');
+define('DB_HOST','149.202.24.76');
 
 $mysqli = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 
 $postjson = json_decode(file_get_contents('php://input'),true);
 
+echo json_encode(array('success'=>false, 'msg'=>"Error, intentelo de nuevo"));
+
 /*if($postjson['mode']=="cargarProductos"){
   
-  $query = mysqli_query($mysqli, "SELECT * FROM ps_product_lang WHERE id_product =1");
+  $query = mysqli_query($mysqli, "SELECT * FROM ps_product_lang WHERE id_product ='1'");
   $data = array();
 
   while($row=mysqli_fetch_array($query)){
